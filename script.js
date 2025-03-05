c457f8aee6e09a33c13565395b0e14504bfdf4aa
iscolso <josecolso24@gmail.com>
2025-03-05 19:47:30 +0100

    agrego dinamica al portfolio

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function copiarCorreo(){
     document.execCommand("copy");
     alert("email copied: " + emailInput.value);
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const textElement = document.getElementById("typing-text");
+    const text = "A developer and an engineering student.";
+    let index = 0;
+
+    function type(){
+        if (index < text.length){
+            textElement.textContent += text[index];
+            index++;
+            setTimeout(type,100);
+        }
+    }
+
+    type();
+});
+
